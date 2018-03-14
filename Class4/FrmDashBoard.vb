d4f18641fb38a83a45c4ad90729f0da0a9774d1a
mtotodev05 <mtotodev05@gmail.com>
2018-03-14 08:45:16 +0300

    Updated

--- Class4/FrmDashBoard.vb
+++ Class4/FrmDashBoard.vb
@@ -12,17 +12,36 @@
         CmbNational.Items.Add("Nairobi School")
         CmbNational.Items.Add("TumuTumu Boys")
         CmbNational.Items.Add("Bahati Girls")
-        Count = 0
+        Count = 1
     End Sub
 
     Private Sub BtnSubCounty_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSubCounty.Click
 
         LstSelected.Items.Add(CmbSubCounties.Text)
         Count = Count + 1
-        If Count < 3 Then
-            MsgBox("You are Required to Enter 3 Choices")
+        If (Count > 2) Then
+            MsgBox("You are Required to Choose 2 Choices")
+            BtnSubCounty.Enabled = False
 
         End If
 
+    End Sub
+
+    Private Sub BtnCounty_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCounty.Click
+        LstSelected.Items.Add(CmbCounty.Text)
+        Count = Count + 1
+        If (Count > 3) Then
+            MsgBox("You are Required to Choose 3 Choices")
+            BtnCounty.Enabled = False
+        End If
+    End Sub
+
+    Private Sub BtnNational_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnNational.Click
+        LstSelected.Items.Add(CmbNational.Text)
+        Count = Count + 1
+        If (Count > 4) Then
+            MsgBox("You are Required to Choose 4 Choices")
+            BtnNational.Enabled = False
+        End If
     End Sub
 End Class
